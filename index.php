a8be9ba0e933e9098ae7ad54c351a41ea1042db4
Henry781029 <61300603+Henry781029@users.noreply.github.com>
2020-11-02 16:24:26 -0500

    formulario

--- index.php
+++ index.php
@@ -4,22 +4,47 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tienda de MARIANA</title>
-    <link rel="stylesheet" href="img\niñadulce.png">
+    <link rel="icon" type="img/png" href="img\niñadulce.png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 
 <body>
 
     <header>
-    
-    
+      <div>
+        <img src="img/niñadulce.png" width="50" height="50" alt="imagen1">
+        <h1>Tienda de Mariana</h1>
+
+      </div>
     
     </header>
 
     <main>
-    
-    
-    
+      
+<form class="mt-5" action="" method="">
+  <div class="form-group">
+    <label for="">NOMBRE</label>
+    <input type="text" placeholder="Nompre producto" class="form-control">
+  </div>
+  <div class="form-group">
+    <label for="">MARCA</label>
+    <input type="text" placeholder="Marca Producto" class="form-control">
+  </div>
+  <div class="form-group">
+    <label for="">PRECIO</label>
+    <input type="text" placeholder="Ingrese Precio" class="form-control">
+  </div>
+  <div class="form-group">
+    <label for="">DESCRIPCION</label>
+    <input type="text" placeholder="Descripcion del producto" class="form-control">
+  </div>
+  <div class="form-group">
+    <button class="btn btn-danger mt-5">Registrar</button>  
+  </div>
+  
+
+</form>
+
     
     </main>
 
